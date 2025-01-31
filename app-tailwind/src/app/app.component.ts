8ec6e90e0716efaf9926991071ea3fa7cbe77615
robots4life <34193243+robots4life@users.noreply.github.com>
2025-01-31 19:52:14 +0100

    dashboard demo tailwind

--- app-tailwind/src/app/app.component.ts
+++ app-tailwind/src/app/app.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  LucideAngularModule,
+  Menu,
+  Bell,
+  User,
+  Home,
+  Settings,
+  BarChart2,
+  Users,
+  Search,
+} from 'lucide-angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [LucideAngularModule, CommonModule],
 })
 export class AppComponent {
-  title = 'app-tailwind';
+  readonly icons = {
+    menu: Menu,
+    bell: Bell,
+    user: User,
+    home: Home,
+    settings: Settings,
+    chart: BarChart2,
+    users: Users,
+    search: Search,
+  };
+
+  isSidebarOpen = true;
+
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
 }
